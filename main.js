a64dd3c3166eaccd01e9850101a87ab5a82b8108
cros6y6raga <crosbybraga@gmail.com>
2023-02-13 13:36:50 +0300

    Switch/Case - Bug Fixing #6

--- main.js
+++ main.js
@@ -148,5 +148,29 @@
 //     return "Hello, " + name + '!';
 // };
 
-items = []
-items.push({a: "b", c: "d"})
+// items = []
+// items.push({a: "b", c: "d"})
+
+function evalObject(value) {
+    var result = 0;
+    switch (value.operation) {
+        case'+':
+            result = value.a + value.b
+            break;
+        case'-':
+            result = value.a - value.b
+            break;
+        case'/':
+            result = value.a / value.b
+            break;
+        case'*':
+            result = value.a * value.b
+            break;
+        case'%':
+            result = value.a % value.b
+            break;
+        case'^':
+            result = Math.pow(value.a, value.b);
+    }
+    return result;
+}
